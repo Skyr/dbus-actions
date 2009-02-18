829a537a667777a59ee476973af8ec7c0e73f4cb
Stefan Schlott <stefan@ploing.de>
2009-02-18 10:24:23 +0100

    Use Dialog instead of Window. Refactored to work both stand-alone and as module

--- src/modules/holux/gpslogdownloader.py
+++ src/modules/holux/gpslogdownloader.py
@@ -7,6 +7,7 @@
 # idProduct: ea60 = 60000dec
 
 
+import logging 
 import os
 import sys
 import time
@@ -25,7 +26,14 @@ except:
     sys.exit(1)
 
 
+gladePath=None
+
 def findGladeFile(filename):
+    global gladePath
+    if gladePath:
+        fullname=os.path.join(gladePath,filename)
+        if os.path.isfile(fullname):
+            return fullname        
     fullname=os.path.join("/usr/share/gpslog-downloader",filename)
     if os.path.isfile(fullname):
         return fullname
@@ -38,66 +46,11 @@ def findGladeFile(filename):
     return filename
 
 
-class SettingsDialog:
-    def __init__(self, mainDlg):
-        self.mainDlg = mainDlg
-        # Set the Glade file
-        self.gladefile = findGladeFile("settingsdialog.glade")
-        self.wTree = gtk.glade.XML(self.gladefile, "dialogSettings")
-        # Autoconnect events
-        self.wTree.signal_autoconnect(self)
-        # Add each widget as an attribute of object
-        for w in self.wTree.get_widget_prefix(''):
-            name = w.get_name()
-            # make sure we don't clobber existing attributes
-            assert not hasattr(self, name)
-            setattr(self, name, w)
-        # Set default data
-        self.dirchooser.set_current_folder(self.mainDlg.dataPath)
-        self.entryFilenameTemplate.set_text(self.mainDlg.dataFilenameTemplate)
-        self.entryDownloadCmd.set_text(self.mainDlg.downloadCmd)
-        self.entryEraseCmd.set_text(self.mainDlg.eraseCmd)
-        self.cbEraseOnDevice.set_active(self.mainDlg.eraseOnDevice)
-        self.entryDefaultDevice.set_text(self.mainDlg.defaultDevice)
-    
-    def on_buttonOk_clicked(self, widget):
-        self.dialogSettings.hide()
-        # Transmit entered data
-        self.mainDlg.dataPath=self.dirchooser.get_filename()
-        self.mainDlg.dataFilenameTemplate=self.entryFilenameTemplate.get_text()
-        self.mainDlg.downloadCmd=self.entryDownloadCmd.get_text()
-        self.mainDlg.eraseCmd=self.entryEraseCmd.get_text()
-        self.mainDlg.eraseOnDevice=self.cbEraseOnDevice.get_active()
-        self.mainDlg.defaultDevice=self.entryDefaultDevice.get_text()
-        # Store and apply
-        self.mainDlg.storeSettings()
-        self.mainDlg.setDefaultValues()
-    
-    def on_buttonCancel_clicked(self, widget):
-        self.dialogSettings.hide()
-
-
-class ImportDialog:
-    """Main dialog"""
-    def __init__(self, deviceName):
-        self.deviceName=deviceName
-        # Set the Glade file
-        self.gladefile = findGladeFile("importwindow.glade")
-        self.wTree = gtk.glade.XML(self.gladefile, "windowImport")
-        # Autoconnect events
-        self.wTree.signal_autoconnect(self)
+class Settings:
+    def __init__(self):
         # Get GConf client
         self.conf = gconf.client_get_default()
         self.confAppKey = "/apps/gpslog-downloader"
-        # Add each widget as an attribute of object
-        for w in self.wTree.get_widget_prefix(''):
-            name = w.get_name()
-            # make sure we don't clobber existing attributes
-            assert not hasattr(self, name)
-            setattr(self, name, w)
-        # Set default values
-        self.getSettings()
-        self.setDefaultValues()
 
     def getSettings(self):
         self.eraseOnDevice = self.conf.get_bool(self.confAppKey+"/erase_on_device")
@@ -115,31 +68,92 @@ class ImportDialog:
         self.conf.set_string(self.confAppKey+"/data_filename_template",self.dataFilenameTemplate)
         self.conf.set_string(self.confAppKey+"/default_device",self.defaultDevice)
     
+
+class SettingsDialog:
+    def __init__(self, settings):
+        self.settings = settings
+        # Set the Glade file
+        self.gladefile = findGladeFile("settingsdialog.glade")
+        self.wTree = gtk.glade.XML(self.gladefile, "dialogSettings")
+        # Autoconnect events
+        self.wTree.signal_autoconnect(self)
+        # Add each widget as an attribute of object
+        for w in self.wTree.get_widget_prefix(''):
+            name = w.get_name()
+            # make sure we don't clobber existing attributes
+            assert not hasattr(self, name)
+            setattr(self, name, w)
+        # Set default data
+        self.dirchooser.set_current_folder(self.settings.dataPath)
+        self.entryFilenameTemplate.set_text(self.settings.dataFilenameTemplate)
+        self.entryDownloadCmd.set_text(self.settings.downloadCmd)
+        self.entryEraseCmd.set_text(self.settings.eraseCmd)
+        self.cbEraseOnDevice.set_active(self.settings.eraseOnDevice)
+        self.entryDefaultDevice.set_text(self.settings.defaultDevice)
+    
+    def on_buttonOk_clicked(self, widget):
+        self.dialogSettings.hide()
+        # Transmit entered data
+        self.settings.dataPath=self.dirchooser.get_filename()
+        self.settings.dataFilenameTemplate=self.entryFilenameTemplate.get_text()
+        self.settings.downloadCmd=self.entryDownloadCmd.get_text()
+        self.settings.eraseCmd=self.entryEraseCmd.get_text()
+        self.settings.eraseOnDevice=self.cbEraseOnDevice.get_active()
+        self.settings.defaultDevice=self.entryDefaultDevice.get_text()
+        # Store and apply
+        self.settings.storeSettings()
+    
+    def on_buttonCancel_clicked(self, widget):
+        self.dialogSettings.hide()
+
+
+class ImportDialog:
+    """Main dialog"""
+    def __init__(self, deviceName):
+        self.deviceName=deviceName
+        self.logger = logging.getLogger("Module")        
+        # Set the Glade file
+        #self.gladefile = findGladeFile("importwindow.glade")
+        #self.wTree = gtk.glade.XML(self.gladefile, "windowImport")
+        self.gladefile = findGladeFile("importdialog.glade")
+        self.wTree = gtk.glade.XML(self.gladefile, "dialogImport")
+        # Autoconnect events
+        self.wTree.signal_autoconnect(self)
+        # Add each widget as an attribute of object
+        for w in self.wTree.get_widget_prefix(''):
+            name = w.get_name()
+            # make sure we don't clobber existing attributes
+            assert not hasattr(self, name)
+            setattr(self, name, w)
+        # Set default values
+        self.settings=Settings()
+        self.settings.getSettings()
+        self.setDefaultValues()
+
     def setDefaultValues(self):
-        self.cbErase.set_active(self.eraseOnDevice)
-        self.filechooser.set_current_folder(self.dataPath)
-        dataFilename=time.strftime(self.dataFilenameTemplate)
+        self.cbErase.set_active(self.settings.eraseOnDevice)
+        self.filechooser.set_current_folder(self.settings.dataPath)
+        dataFilename=time.strftime(self.settings.dataFilenameTemplate)
         self.filechooser.set_current_name(dataFilename)
 
-    def on_windowImport_destroy(self, *args):
-        gtk.main_quit()
-
     def on_buttonConfigure_clicked(self, *args):
-        dlg = SettingsDialog(self)
-        dlg.dialogSettings.show()
+        cfgdlg = SettingsDialog(self.settings)
+        if cfgdlg.dialogSettings.run()==gtk.RESPONSE_OK:
+            self.setDefaultValues()
+        cfgdlg.dialogSettings.destroy()
 
     def on_buttonCancel_clicked(self, *args):
-        gtk.main_quit()
+        self.dialogImport.response(gtk.RESPONSE_CANCEL)
 
     def on_buttonOk_clicked(self, *args):
         if not self.deviceName:
-            self.deviceName=self.defaultDevice
+            self.deviceName=self.settings.defaultDevice
         dataFilename=self.filechooser.get_filename()
         exportAndQuit=True
         if os.path.exists(dataFilename):
             if not os.path.isfile(dataFilename):
                 return
-            dlg = gtk.MessageDialog(parent=self.windowImport,type=gtk.MESSAGE_WARNING,buttons=gtk.BUTTONS_YES_NO)
+            dlg = gtk.MessageDialog(parent=self.dialogImport,type=gtk.MESSAGE_WARNING,buttons=gtk.BUTTONS_YES_NO)
             dlg.set_title("File exists")
             dlg.set_markup("File %s already exists - overwrite?" % (dataFilename))
             if dlg.run()!=gtk.RESPONSE_YES:
@@ -150,21 +164,33 @@ class ImportDialog:
                 'filename' : "%s" % (dataFilename),
                 'device' : "%s" % (self.deviceName),  
             }
-            downloadCmd = self.downloadCmd % paramMap 
-            eraseCmd = self.eraseCmd % paramMap
-            print "Running %s" % (downloadCmd)
+            downloadCmd = self.settings.downloadCmd % paramMap 
+            eraseCmd = self.settings.eraseCmd % paramMap
+            self.logger.debug("Running %s" % (downloadCmd))
             result=commands.getstatusoutput(downloadCmd)
-            print "Result: %s" % (result[1])
+            self.logger.debug("Result: %s" % (result[1]))
             if result[0]==0:
-                print "Running %s" % (eraseCmd)
+                self.logger.debug("Running %s" % (eraseCmd))
                 commands.getstatusoutput(eraseCmd)
+                self.dialogImport.response(gtk.RESPONSE_OK)
             else:
-                dlg = gtk.MessageDialog(parent=self.windowImport,type=gtk.MESSAGE_ERROR,buttons=gtk.BUTTONS_OK)
+                dlg = gtk.MessageDialog(parent=self.dialogImport,type=gtk.MESSAGE_ERROR,buttons=gtk.BUTTONS_OK)
                 dlg.set_title("Error downloading data")
                 dlg.set_markup("Error downloading data:\n%s" % (result[1]))
                 dlg.run()
-            # Quit application
-            gtk.main_quit()
+                self.dialogImport.response(gtk.RESPONSE_REJECT)
+        else:
+            self.dialogImport.response(gtk.RESPONSE_REJECT)
+
+
+def run(deviceName,showConfig):
+    result=gtk.RESPONSE_REJECT
+    while result==gtk.RESPONSE_REJECT:
+        dlgImport = ImportDialog(deviceName)
+        if showConfig:
+            dlgImport.cfgButtonBox.show()
+        result=dlgImport.dialogImport.run()
+        dlgImport.dialogImport.destroy()
 
 
 if __name__ == "__main__":
@@ -172,5 +198,4 @@ if __name__ == "__main__":
     parser.add_option("-d", "--device", dest="deviceName",
                   help="Device name of GPS device", default=None)
     (options, args) = parser.parse_args()
-    dlgImport = ImportDialog(options.deviceName)
-    gtk.main()
+    run(options.deviceName,True)
--- src/modules/holux/holux.py
+++ src/modules/holux/holux.py
@@ -1,6 +1,7 @@
 import commands
 import dbus
 import dbusactions.module
+from . import gpslogdownloader
 
 class Module(dbusactions.module.Module):
     def __init__(self,moduleParams):
@@ -8,6 +9,17 @@ class Module(dbusactions.module.Module):
         self.moduleName = "Holux GPS trigger"
         self.iconFilename = "holuxicon.png"
         self.interfaceList = [('org.freedesktop.Hal.Manager','DeviceAdded')]
+        gpslogdownloader.gladePath=self.modulePath
+
+    def isConfigurable(self):
+        return True
+    
+    def configureDialog(self,parentWidget):
+        settings = gpslogdownloader.Settings()
+        settings.getSettings()
+        cfgdlg = gpslogdownloader.SettingsDialog(settings)
+        cfgdlg.dialogSettings.run()
+        cfgdlg.dialogSettings.destroy()
 
     def dbusSystemEvent(self, *args, **keywordArgs):
         deviceName=args[0]
@@ -20,6 +32,7 @@ class Module(dbusactions.module.Module):
                 parentDeviceIntf=dbus.Interface(parentDevice,dbus_interface='org.freedesktop.Hal.Device')
                 if parentDeviceIntf.PropertyExists("usb.vendor_id") and parentDeviceIntf.PropertyExists("usb.product_id") and \
                     parentDeviceIntf.GetPropertyInteger("usb.vendor_id")==4292 and parentDeviceIntf.GetPropertyInteger("usb.product_id")==60000:
-                    cmd="/home/sts/source/gpslog-downloader/src/gpslog-downloader.py -d %s" % deviceIntf.GetPropertyString("serial.device")
-                    self.logger.debug("Executing %s" % cmd)
-                    result=commands.getstatusoutput(cmd)
+                    #cmd="/home/sts/source/gpslog-downloader/src/gpslog-downloader.py -d %s" % deviceIntf.GetPropertyString("serial.device")
+                    #self.logger.debug("Executing %s" % cmd)
+                    #result=commands.getstatusoutput(cmd)
+                    gpslogdownloader.run(deviceIntf.GetPropertyString("serial.device"),False)
