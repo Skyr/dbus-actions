6a5ac9e06c9c06661dd577e82751bace1627c445
Stefan Schlott <stefan@ploing.de>
2009-02-27 12:23:05 +0100

    Decluttering. Prepare comment in constructor to match documentation

--- src/modules/test/__init__.py
+++ src/modules/test/__init__.py
@@ -6,11 +6,12 @@ import dbusactions.module
 
 class Module(dbusactions.module.Module):
     def __init__(self,moduleParams):
-        #dbusactions.module.Module.__init__(self,systemBus,sessionBus)
         super(Module,self).__init__(moduleParams)
-        self.moduleName = "Test module"
+        self.moduleName = "Test module"        
+        # Uncomment one of these:        
+        #self.inferfaceList = []
         #self.interfaceList = ['org.gnome.Tomboy.RemoteControl']
-        self.interfaceList = [('im.pidgin.purple.PurpleInterface','ConversationSwitched')]
+        #self.interfaceList = [('im.pidgin.purple.PurpleInterface','ConversationSwitched')]
 
     def isConfigurable(self):
         return False
@@ -22,7 +23,5 @@ class Module(dbusactions.module.Module):
         keywords=[]
         for kw in kwargs.keys():
             keywords.append("%s=%s"%(kw,kwargs[kw]))
-        #eventstr="%s.%s(%s)" % (kwargs['dbus_interface'],kwargs['member'],",".join(params))
-        #print "dbus session bus: %s" % eventstr
         print "Keywords: " + ",".join(keywords)
         print "Params:   " + ",".join(params)
